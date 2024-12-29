d3db2d51ba64b1f1ec2061143c270c3380f8511b
Fede <federicogonzalez@sportclub.team>
2024-12-29 18:42:12 -0300

    Version con dependencias sin .proto aun

---
+++ src/main/java/com/example/prueba_grpc/PruebaGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueba_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaGrpcApplication.class, args);
+    }
+
+}
